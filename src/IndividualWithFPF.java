9915d5b292cec6e4031e07989b6e637672dda134
Christoph Pölt <c.poelt@campus.lmu.de>
2025-04-28 18:06:21 +0200

    final version

--- src/IndividualWithFPF.java
+++ src/IndividualWithFPF.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 
+@Getter
 public class IndividualWithFPF {
+    private final double[] genome;
+    private final double fpfValue;
+
+    public IndividualWithFPF(Individual individual, double fpfValue) {
+        this.genome = individual.getGenome();
+        this.fpfValue = fpfValue;
+    }
+
+    @JsonProperty("genome")
+    public double[] getGenome() {
+        return genome;
+    }
+
+    @JsonProperty("fpfValue")
+    public double getFpfValue() {
+        return fpfValue;
+    }
 }
